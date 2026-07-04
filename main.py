41e9595588dc27915eeb00757d789d1d03df0aff
RobertsTr <troksaroberts@gmail.com>
2026-07-05 02:17:27 +0300

    Added login page and capcha without checking it completion in backend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -22,3 +23,21 @@ def signup():
         return render_template("home_signuppage.html")
     except:
         return render_template("error.html")
+    
+
+def verify_token(token, ip):
+    data = {
+        "secret": "",
+        "response": token,
+        "remoteip": ip,
+        "sitekey": "0ff041fe-1418-4c74-8bca-10882266eb3a",
+    }
+    j = requests.post(
+        "https://api.hcaptcha.com/siteverify",
+        data=data,
+        timeout=5,
+    ).json()
+    return (True, []) if j.get("success") else (
+        False,
+        j.get("error-codes", []),
+    )
